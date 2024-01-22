f3115be21a441887f8d244bd73ee867b76b28875
Rishi Gundakaram <rishi.gundakaram@gmail.com>
2024-01-21 22:06:12 -0500

    started work on parsing retrosheet

--- utils/retro_playbyplay.py
+++ utils/retro_playbyplay.py
@@ -4,19 +4,18 @@ import json
 import pandas as pd
 import os
 import numpy as np
+from alive_progress import alive_bar
 
-
+ignore = set(["save", "pitches", "oscorer", "wp", "lp", "save", "timeofgame", "wp", "lp", "gwrbi", "edittime", "howscored", "howentered", "inputter", "scorer", "translator"])
 def parse_id(game, lst):
     game["game_id"] = lst[1]
 
 
 def parse_info(game, lst):
-    if lst[1] == "visteam":
-        game["away_team"] = lst[2]
-    elif lst[1] == "hometeam":
-        game["home_team"] = lst[2]
-    elif lst[1] == "date":
-        game["date"] = lst[2]
+    global ignore
+    if lst[1] not in ignore:
+        game[lst[1]] = lst[2] 
+    
 
 
 def parse_start_sub(game, lst):
@@ -187,36 +186,6 @@ def aggregate_EVX_files(root_directory):
     return all_games
 
 
-def add_scores_to_games(all_games, gamelogs_dir):
-    blocks = []
-    for root, dirs, files in os.walk(gamelogs_dir):
-        for file in files:
-            if file.endswith(".txt"):
-                file_path = os.path.join(root, file)
-                blocks.append(pd.read_csv(file_path, header=None))
-    game_logs = pd.concat(blocks)
-
-    # switch columns where home team bats first
-    game_logs["game_id"] = np.where(
-        game_logs.iloc[:, 159] == "HTBF",  # Condition
-        game_logs[3].astype(str)
-        + game_logs[0].astype(str)
-        + game_logs[1].astype(str),  # True case
-        game_logs[6].astype(str)
-        + game_logs[0].astype(str)
-        + game_logs[1].astype(str),  # False case
-    )
-    # map {game_id : [home_score, away_score]}
-    scores = zip(game_logs[10].astype(int), game_logs[9].astype(int))
-    run_mapping = dict(zip(game_logs["game_id"], scores))
-    all_games = [game for game in all_games if game["game_id"] in run_mapping]
-    for game in all_games:
-        game["home_score"] = run_mapping[game["game_id"]][0]
-        game["away_score"] = run_mapping[game["game_id"]][1]
-
-    return all_games
-
-
 def EVX_json_to_csv(path):
     with open(path, "r") as f:
         all_games = json.load(f)
@@ -234,19 +203,37 @@ def EVX_json_to_csv(path):
     df = pd.DataFrame(rows)
     df.to_csv("plays.csv")
 
+# write a function, that takes in a list of games which is a list of dictionaries and returns a dataframe
+def games_to_df(games):
+    rows = []
+    # wrap this in a timer
+    with alive_bar(len(games)) as bar:
+        for game in games:
+            for play in game.get("plays", []):
+                rows.append(
+                    {
+                        'date': game['date'],
+                        'home_team': game['home_team'],
+                        'away_team': game['away_team'],
+                        'temp': game['temp'],
+                        'winddir': game['winddir'],
+                        'windspeed': game['windspeed'],
+                        'fieldcond': game['fieldcond'],
+                        'precip': game['precip'],
+                        'start_time': game['start_time'],
+                        'attendance': game['attendance'],
+                        "pitcher": play["pitcher"],
+                        "batter": play["batter"],
+                        "outcome": play["outcome"],
+                    }
+                )
+            bar()
+    df = pd.DataFrame(rows)
+    return df
 
 if __name__ == "__main__":
-    playbyplay_directory = "/home/projects/baseball-MCS/data/raw/playbyplay"  # Replace with your actual root directory
-    gamelogs_directory = "/home/projects/baseball-MCS/data/raw/gamelogs"
+    playbyplay_directory = "/home/projects/baseball-datasets/data/retrosheet/playbyplay"  # Replace with your actual root directory
     output_path = "/home/projects/baseball-MCS/data/intermediate/all_games.json"
     all_games = aggregate_EVX_files(playbyplay_directory)
     print(f"found {len(all_games)} games")
-    all_games = add_scores_to_games(all_games, gamelogs_directory)
-    print(f"scores for {len(all_games)}")
-    # # analyze_outcomes(all_games)
-
-    # # # Save the object as a JSON file
-    with open(output_path, "w+") as f:
-        json.dump(all_games, f)
-    with open(output_path, "r+") as f:
-        test_games = json.load(f)
+    games_to_df(all_games).to_csv("/home/projects/baseball-datasets/data/final/atbats.csv")
